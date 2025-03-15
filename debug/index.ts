b7c269188dfa61d722adc03a68c830463cd9b7ac
tenderjin <112069806+tenderjin@users.noreply.github.com>
2025-03-15 13:59:04 +0800

    Update index.ts

--- debug/index.ts
+++ debug/index.ts
@@ -1,6 +1,6 @@
-import { paseArticleUrl } from '../parse-article-url.ts'
+import { paseArticleUrl } from '../src/parse-article-url.ts'
 
 const url =
-  'http://mp.weixin.qq.com/s?__biz=MzU2MDU4NDE1MQ==&mid=2247528254&idx=1&sn=2a94b1bc766fce1e776d8e20f6f672c0'
+  'http://mp.weixin.qq.com/s?__biz=MzA5MTczNDc1OQ==&mid=2247495053&idx=1&sn=51a92780d46d2b352c99e33bb2351683'
 
 console.log(paseArticleUrl(url))
